0c6e2ebf9e357f8bfbb1d1678c712c0d1cfe20f7
Punith MK <punithmadaiahkumar@gmail.com>
2022-12-06 09:48:19 +0530

    catching exception 2 script

---
+++ Exception Handling/Catching_exception2.py
@@ -0,0 +1,13 @@
+# Catching Info Exception:
+randomList = ['a', 0, 4]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except Exception as e:
+        print("Oops!", e.__class__, "occurred.")
+        print("Next entry.")
+        print()
+print("The reciprocal of", entry, "is", r)
