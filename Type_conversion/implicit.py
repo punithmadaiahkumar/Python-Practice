c67b9a1dc640ae436ffd8af39b3f87019323013b
Punith MK <punithmadaiahkumar@gmail.com>
2022-12-08 09:30:20 +0530

    implicit conversion script

---
+++ Type_conversion/implicit.py
@@ -0,0 +1,8 @@
+integer_number = 456
+float_number = 4.56
+
+new_number = integer_number + float_number
+
+# display new number and Data type of the New Number
+print("Value:",new_number)
+print("Data Type:",type(new_number))
