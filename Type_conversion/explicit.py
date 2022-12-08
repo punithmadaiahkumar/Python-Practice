cead308e88f5cb50817ff4b02cc58313acfb4f59
Punith MK <punithmadaiahkumar@gmail.com>
2022-12-08 09:30:07 +0530

    explicit conversion script

---
+++ Type_conversion/explicit.py
@@ -0,0 +1,18 @@
+str_num = '15'
+int_num = 35
+
+# explicit type conversion
+conv_str = int(str_num)
+
+#summing total:
+sum_total = int_num + conv_str
+
+#printing values:
+
+print("String number:",str_num)
+
+print("Integer number:",int_num)
+
+print("---------------------")
+print("Total Sum:",sum_total)
+print("---------------------")
