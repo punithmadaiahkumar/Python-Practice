90169c3936b0119217a405d172002bb963359ecd
Punith Gowda <punithmadaiahkumar@gmail.comm>
2023-01-05 10:00:46 +0530

    Multiple Number using Lambda

---
+++ Lamda/multiple_number.py
@@ -0,0 +1,7 @@
+a = int(input('Enter 1st number: '))
+b = int(input('Enter 2nd number: '))
+c = int(input('Enter 3rd number: '))
+
+output = lambda a, b, c: a * b * c
+
+print('Multiplication result is: ', output(a, b, c))
