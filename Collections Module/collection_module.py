29c4f87d30025ed1edac72f570ab189af9373847
punithmadaiahkumar <punithmadaiahkumar>
2023-01-19 13:19:37 +0530

    collection script

---
+++ Collections Module/collection_module.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+# With sequence of items
+print(Counter(['B','B','A','B','C','A','B','B','A','C']))
+
+# with dictionary
+count = Counter({'A':3, 'B':5, 'C':2})
+print(count)
+
+xys = count.update(['A', 1])
+print(xys)
