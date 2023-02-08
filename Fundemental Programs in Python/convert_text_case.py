3f55e92d45a93b5a7ffe8bc34786f4fb7fa6afe9
Punith MK <punithmadaiahkumar@gmail.com>
2023-02-08 09:51:03 +0530

    Character Cases in Python

---
+++ Fundemental Programs in Python/convert_text_case.py
@@ -0,0 +1,16 @@
+#Python program to convert all characters
+
+def chracter(s):
+    txt = str(s)
+    print("Uppercase: ",txt.upper())
+    print('------------------------------')
+    print("Lowercase: ",txt.lower())
+    print('------------------------------')
+    print("Capitalize: ",txt.capitalize())
+    print('------------------------------')
+    print("Title: ",txt.title())
+    print('------------------------------')
+
+
+chracter('python program')
+        
