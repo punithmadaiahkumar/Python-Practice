acb71219d95ec97be586d155533eb8b0e74bfa96
Punith MK <punithmadaiahkumar@gmail.com>
2023-02-28 09:32:31 +0530

    identity matrix

---
+++ Fundemental Programs in Python/identity_matrix.py
@@ -0,0 +1,9 @@
+#Python program to print Identity Matrix
+rows=3
+for i in range(0,rows):
+    for j in range(0,rows):
+        if(i==j):
+            print("1",sep=" ",end=" ")
+        else:
+            print("0",sep=" ",end=" ")
+    print()
