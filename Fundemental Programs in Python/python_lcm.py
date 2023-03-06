e5a89e10336a8fd86e25412998ff8126107b42a8
Punith MK <punithmadaiahkumar@gmail.com>
2023-03-06 12:23:39 +0530

    LCM in python

---
+++ Fundemental Programs in Python/python_lcm.py
@@ -0,0 +1,8 @@
+#Python Program to compute LCM 
+num1 = 45
+num2 = 60
+for i in range(max(num1, num2), 1 + (num1 * num2)):
+    if i % num1 == i % num2 == 0:
+        lcm = i
+        break
+print("LCM of", num1, "and", num2, "is", lcm)
