deaa2de8098332dacff2b59fbc0ab690e457b2c2
Punith MK <punithmadaiahkumar@gmail.com>
2023-03-08 11:52:07 +0530

    Python Simple Lambda

---
+++ Fundemental Programs in Python/python_simple_lambda.py
@@ -0,0 +1,5 @@
+x = int(input("Enter the 1st number: "))
+y = int(input("Enter the 2nd number: "))
+z = int(input("Enter the 3rd number: "))
+sum_ = lambda x,y,z : x+y+z
+print("Sum of Total is: ",sum_(x,y,z))
